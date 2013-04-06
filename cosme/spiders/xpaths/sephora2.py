9b4e0eef0d2079f9089317214f128e8cf65d2ef9
Karthik Kumar <akkumar@gmail.com>
2013-04-06 16:28:54 +0530

    move around files

--- cosme/spiders/xpaths/sephora2.py
+++ cosme/spiders/xpaths/sephora2.py
@@ -3,7 +3,7 @@ META = {
 "image" : "//div[@class=\'lateralImagemProduto highslide-gallery\']/img/@src",
 "name" :  "//div[@class=\'tituloDescricaoProduto\']/h1/text()",
 "brand" : "//ul[@class=\'breadCrumb\']/li[1]/a/text()",
-"price" : "//span[@class=\'produtoPrecoVendaPor\']/text()"
+"price" : "//span[@class=\'produtoPrecoVendaPor\']/text()",
 "description" : "//div[@class=\'descricaoProduto\']/p/text()",
 "category" : "//ul[@class=\'breadCrumb\']/li[4]/a/text()",
 "sku" : "//td[@class=\'refSKU\']/span[@class=\'refSKUNumero\']/text()",
