9919d79de6efb906a684aaec39d018af5f5c4cd3
Karthik Kumar <akkumar@gmail.com>
2013-04-07 08:44:35 +0530

    add test

--- test/test_laffayette_comment.py
+++ test/test_laffayette_comment.py
@@ -1,23 +1,23 @@
 import unittest
 
 import logging
-from cosme.spiders.Sephbot import Cosme
 from scrapy.http.request import Request
 from scrapy.http.response.html import HtmlResponse
 from scrapy.selector.lxmlsel import HtmlXPathSelector
 from cosme.spiders.xpaths.xpath_registry import XPathRegistry
+from cosme.spiders.Lafbot import Cosme
 
 logging.debug('creating logger')
 logger = logging.getLogger(__name__)
 
-class TestCommentExtract(unittest.TestCase):
+class TestLaffayetteCommentExtract(unittest.TestCase):
     
 
     xpathRegistry = XPathRegistry()
         
     def test_comment_extraction(self):
         cosme = Cosme()
-        sephora_html = self.load_file('Esmalte Revlon Scented Nail Enamel – MELHOR PREÇO é na Laffayette!.html')
+        sephora_html = self.load_file('Laffayette.html')
         url = 'http://www.laffayette.com.br/produto/esmalte-revlon-scented-nail-enamel-14,7ml.html'
         request = Request(url=url)
         response = HtmlResponse(url=url,
