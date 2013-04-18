e25cbed4faef2254020b42d6260a9102e27e8e54
Karthik Kumar <akkumar@gmail.com>
2013-04-18 19:33:51 +0530

    productId in place

--- cosme/pipes/sepha.py
+++ cosme/pipes/sepha.py
@@ -7,36 +7,39 @@ from  cosme.pipes.default import AbstractSite
 
 class SephaWeb(AbstractSite):
 	def process(self, item, spider, matcher):
-	    if item['url']:
-		item['url'] = item['url'].lower()					
-	    if item['price']: 
-	   # tempPrice = re.search(r'[\d.,]+',str(item['price']))
-	   # tempPrice = tempPrice.group().replace(',','.')
-	   # item['price'] = float(tempPrice)
-		item['price'] = utils.extractPrice(item['price'])
+		if item['url']:
+			item['url'] = item['url'].lower()					
+		if item['price']: 
+			# tempPrice = re.search(r'[\d.,]+',str(item['price']))
+			# tempPrice = tempPrice.group().replace(',','.')
+			# item['price'] = float(tempPrice)
+			item['price'] = utils.extractPrice(item['price'])
 
-	    if item['brand']:
-		tempBrand = item['brand']
-		tempBrand = tempBrand[0]
-		print "########TEMP DOS  ######### %s", tempBrand
-		tempBrand = utils.extractBrand(tempBrand)
-		item['brand'] = tempBrand
+		if item['brand']:
+			tempBrand = item['brand']
+			tempBrand = tempBrand[0]
+			print "########TEMP DOS  ######### %s", tempBrand
+			tempBrand = utils.extractBrand(tempBrand)
+			item['brand'] = tempBrand
 
-	    if item['name']:
-		tempName = item['name']
-		tempName = tempName[0]
-		item['name'] = utils.cleanChars(tempName)
+		if item['name']:
+			tempName = item['name']
+			tempName = tempName[0]
+			item['name'] = utils.cleanChars(tempName)
 
-	    if item['category']:
-		tempCat = item['category']
-		item['category'] =utils.cleanChars(tempCat[0])
-	    if item['image']:
-		temp = item['image'] 
-		temp = temp[0]
-		item['image'] = temp
-	    if item['sku']: 
-		temp = item['sku']
-		temp = temp[0]
-		item['sku'] = utils.extractSku(temp)
-
-	    return item
+		if item['category']:
+			tempCat = item['category']
+			item['category'] =utils.cleanChars(tempCat[0])
+		if item['image']:
+			temp = item['image'] 
+			temp = temp[0]
+			item['image'] = temp
+		if item['sku']: 
+			temp = item['sku']
+			temp = temp[0]
+			item['sku'] = utils.extractSku(temp)
+		if item['product_id']:
+			temp = item['product_id']
+			comment_url = 'http://www.sepha.com.br/comentario/produto/id/%s/pagina/1' % (temp[0])
+			# TODO: Make a call to comment_url and extract from the same.
+		return item
